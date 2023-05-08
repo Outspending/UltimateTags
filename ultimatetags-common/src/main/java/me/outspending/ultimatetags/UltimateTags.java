209c64a8bb34dcd85c475dc4daea4c195b312ca1
Outspending <Dxrippy15@gmail.com>
2023-05-08 16:53:18 -0600

    Initial commit

---
+++ ultimatetags-common/src/main/java/me/outspending/ultimatetags/UltimateTags.java
@@ -0,0 +1,16 @@
+package me.outspending.ultimatetags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UltimateTags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
